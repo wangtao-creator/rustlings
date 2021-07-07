9ed746cf636c0e2fd9ade71b52decc9d7d777141
wt <2521820396@qq.com>
2021-07-07 14:36:19 +0800

    修改

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -1,13 +1,15 @@
 // structs1.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
+
 
 struct ColorClassicStruct {
+    name:String,
+    hex:String,
     // TODO: Something goes here
 }
 
-struct ColorTupleStruct(/* TODO: Something goes here */);
+struct ColorTupleStruct(String,String);
 
 #[derive(Debug)]
 struct UnitStruct;
@@ -20,7 +22,10 @@ mod tests {
     fn classic_c_structs() {
         // TODO: Instantiate a classic c struct!
         // let green =
-
+        let green = ColorClassicStruct{
+            name:String::from("green"),
+            hex:String::from("#00FF00"),
+        };
         assert_eq!(green.name, "green");
         assert_eq!(green.hex, "#00FF00");
     }
@@ -29,7 +34,7 @@ mod tests {
     fn tuple_structs() {
         // TODO: Instantiate a tuple struct!
         // let green =
-
+        let green = ColorTupleStruct("green".to_string(),"#00FF00".to_string());
         assert_eq!(green.0, "green");
         assert_eq!(green.1, "#00FF00");
     }
@@ -38,6 +43,7 @@ mod tests {
     fn unit_structs() {
         // TODO: Instantiate a unit struct!
         // let unit_struct =
+        let unit_struct = UnitStruct{};
         let message = format!("{:?}s are fun!", unit_struct);
 
         assert_eq!(message, "UnitStructs are fun!");
