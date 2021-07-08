c2f2ee64c3a1c2e3dcc107bb96edaa35c69a43ae
wt <2521820396@qq.com>
2021-07-08 18:09:32 +0800

    7.8

--- exercises/standard_library_types/box1.rs
+++ exercises/standard_library_types/box1.rs
@@ -9,18 +9,18 @@
 // frequently found in functional programming languages. Each item in a cons list contains two
 // elements: the value of the current item and the next item. The last item is a value called `Nil`.
 //
-// Step 1: use a `Box` in the enum definition to make the code compile
+// Step 1: use a `Box` in th e enum definition to make the code compile
 // Step 2: create both empty and non-empty cons lists by replacing `unimplemented!()`
 //
 // Note: the tests should not be changed
 //
 // Execute `rustlings hint box1` for hints :)
 
-// I AM NOT DONE
+
 
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
@@ -33,11 +33,11 @@ fn main() {
 }
 
 pub fn create_empty_list() -> List {
-    unimplemented!()
+    List::Nil
 }
 
 pub fn create_non_empty_list() -> List {
-    unimplemented!()
+    List::Cons(1,Box::new(List::Nil))
 }
 
 #[cfg(test)]
